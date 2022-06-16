fbef26e618d97275c2fb6cb293e5a0cf1c0dd84a
Mihir Mulay <55177881+MihirMulay2001@users.noreply.github.com>
2022-06-16 21:09:03 +0530

    addedd abi and contract address for ganache

--- src/App.js
+++ src/App.js
@@ -1,68 +1,65 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Router from './Router';
-import { ethers } from 'ethers';
-import ContractJson from './assets/abi/contract.json';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Router from "./Router";
+import { ethers } from "ethers";
+import ContractJson from "./assets/abi/Factory.json";
 
-const CONTRACT_ADDRESS = ''
+const CONTRACT_ADDRESS = "0x95574ff0d2F8347b919d7a9687632b86D8760e68";
 
 function App() {
-  const [account,setAccount] = useState(null)
-  const [contract, setContract] = useState(null)
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState(null);
 
-
-  const checkWalletIsConnected = () => { 
-    const {ethereum} = window;
-    if(ethereum){
+  const checkWalletIsConnected = () => {
+    const { ethereum } = window;
+    if (ethereum) {
       console.log("We are good to go!");
       return true;
-      
-    }else{
-        alert("Make sure you have metamask installed")
-        return false;
+    } else {
+      alert("Make sure you have metamask installed");
+      return false;
     }
-  }
-
-  const getContract = async () => {
-
-    try{
-      const {ethereum} = window;
-      if(ethereum){
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner();
-        const _contract = new ethers.Contract(CONTRACT_ADDRESS, ContractJson.abi, signer) 
-        setContract(_contract)  
-      }else{
-        console.log("Object doesn't exist");
-      }
-    }catch(e){
-      console.log(e);
-    }
-    
   };
 
+  const getContract = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const _contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          ContractJson.abi,
+          signer
+        );
+        setContract(_contract);
+      } else {
+        console.log("Object doesn't exist");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  function onAccountChange (accounts) {
-      setAccount(accounts[0]);
-      console.log(accounts[0]);
+  function onAccountChange(accounts) {
+    setAccount(accounts[0]);
+    console.log(accounts[0]);
   }
 
-
-  useEffect(()=>{
-    if(checkWalletIsConnected()){
-      // getContract()
+  useEffect(() => {
+    if (checkWalletIsConnected()) {
+      getContract();
       console.log("wallet connected");
     }
-    window.ethereum.on('accountsChanged', onAccountChange)
+    window.ethereum.on("accountsChanged", onAccountChange);
     // return(()=>{
     //   window.ethereum.off('accountsChanged',onAccountChange)
     // })
-  },[])
-
+  }, []);
 
   return (
     <div className="App">
-      <Router account={account} contract={contract} setAccount={setAccount}/>      
+      <Router account={account} contract={contract} setAccount={setAccount} />
     </div>
   );
 }
